fe28e5cc8b19c7153c1f85a17edfd717f3d26141
Owen James <orjames@berkeley.edu>
2019-01-16 14:16:11 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
 //array with four cards in it
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+},
+];
 
 //array with cards that have been selected
 var cardsInPlay = [];
@@ -19,12 +40,16 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 checkForMatch();
 }
 
 flipCard(0);
 flipCard(2);
+
