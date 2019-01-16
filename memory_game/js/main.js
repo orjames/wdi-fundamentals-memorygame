93197851816e04b67c91b383be8465aca8e56d9b
Owen James <orjames@berkeley.edu>
2019-01-16 11:22:24 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,23 +6,25 @@ var cards = ["queen", "queen", "king", "king"];
 //array with cards that have been selected
 var cardsInPlay = [];
 
-//selects card one as the first in the cards array
-var cardOne = cards[0];
-//places cardOne into the cardsInPlay array
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-//checks that the length of cards in cardsInPlay is 2
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} 
-	else {
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+	if (cardsInPlay.length < 2) {
+		return;
+	}
+	else if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
